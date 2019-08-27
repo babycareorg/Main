46ed8c155c01e6a7b7fdc360ad3a7c9bc9292171
kevin <songkun666@outlook.com>
2019-08-27 19:17:32 +0800

    添加登录状态

--- app/src/main/java/cn/bgbsk/babycare/global/Data.java
+++ app/src/main/java/cn/bgbsk/babycare/global/Data.java
@@ -8,11 +8,13 @@ public class Data extends Application {
     private static String url;
     private static String username;
     private static String phone;
+    private static int loginStatus;
 
     @Override
     public void onCreate() {
         url = "https://babycare.bgbsk.cn";
         username = "";
+        loginStatus = 0;
         phone = "未连接";
         super.onCreate();
     }
@@ -26,13 +28,20 @@ public class Data extends Application {
         return username;
     }
 
-
     public static String getPhone() {
         return phone;
     }
 
     public static void setUsername(String username) {
         Data.username = username;
+    }
+
+    public static int getLoginStatus() {
+        return loginStatus;
+    }
+
+    public static void setLoginStatus(int loginStatus) {
+        Data.loginStatus = loginStatus;
     }
 
     public static void setPhone(String phone) {

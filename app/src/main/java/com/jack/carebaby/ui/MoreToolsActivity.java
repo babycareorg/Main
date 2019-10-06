a6966f574e10c092608386f79d170e0a76b5bb0a
jckcc <2508074836@qq.com>
2019-10-06 16:50:29 +0800

    更多页所有功能

--- app/src/main/java/com/jack/carebaby/ui/MoreToolsActivity.java
+++ app/src/main/java/com/jack/carebaby/ui/MoreToolsActivity.java
@@ -1,6 +1,7 @@
 package com.jack.carebaby.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -38,10 +39,10 @@ public class MoreToolsActivity extends Activity {
     /**
      * 用于填充contentListAdapter的数据源。
      */
-    private String[] contentItems = {"指南针", "温度计", "日历",
-            "闹钟", "天气", "录音机", "计算器",
-            "出行运动", "镜子", "音乐", "视频",
-            "博客", "清理", "学习", "其他",
+    private String[] contentItems = {"微信", "设置", "日历",
+            "闹钟", "记事本", "录音机", "计算器",
+            "浏览器", "FM收音机", "音乐", "视频",
+            "微博", "清理", "读书", "其他",
             "更多精彩"};
 
     @Override
@@ -71,6 +72,170 @@ public class MoreToolsActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String text = contentItems[position];
+                switch (position){
+                    case 0:
+                        Intent intent = getPackageManager().getLaunchIntentForPackage("com.tencent.mm");
+                        if (intent != null) {
+                            startActivity(intent);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装微信哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 1:
+                        Intent intent1 = getPackageManager().getLaunchIntentForPackage("com.android.settings");
+                        if (intent1 != null) {
+                            startActivity(intent1);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装设置哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 2:
+                        Intent intent2 = getPackageManager().getLaunchIntentForPackage("com.google.android.calendar");
+                        if (intent2 != null) {
+                            startActivity(intent2);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装日历哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 3:
+                        Intent intent3 = getPackageManager().getLaunchIntentForPackage("com.android.deskclock");
+                        if (intent3 != null) {
+                            startActivity(intent3);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装闹钟哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 4:
+                        Intent intent4 = getPackageManager().getLaunchIntentForPackage("com.example.android.notepad");
+                        if (intent4 != null) {
+                            startActivity(intent4);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装记事本哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 5:
+                        Intent intent5 = getPackageManager().getLaunchIntentForPackage("com.android.soundrecorder");
+                        if (intent5 != null) {
+                            startActivity(intent5);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装录音机哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 6:
+                        Intent intent6 = getPackageManager().getLaunchIntentForPackage("com.android.calculator2");
+                        if (intent6 != null) {
+                            startActivity(intent6);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装计算器哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 7:
+                        Intent intent7 = getPackageManager().getLaunchIntentForPackage("com.android.browser");
+                        if (intent7 != null) {
+                            startActivity(intent7);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装浏览器哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 8:
+                        Intent intent8 = getPackageManager().getLaunchIntentForPackage("com.huawei.android.FMRadio");
+                        if (intent8 != null) {
+                            startActivity(intent8);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装收音机哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 9:
+                        Intent intent9 = getPackageManager().getLaunchIntentForPackage("com.android.mediacenter");
+                        if (intent9 != null) {
+                            startActivity(intent9);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装音乐哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 10:
+                        Intent intent10 = getPackageManager().getLaunchIntentForPackage("com.qiyi.video");
+                        if (intent10 != null) {
+                            startActivity(intent10);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装爱奇艺视频哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 11:
+
+                        Intent intent11 = getPackageManager().getLaunchIntentForPackage("com.sina.weibo");
+                        if (intent11 != null) {
+                            startActivity(intent11);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装微博哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 12:
+
+                        Intent intent12 = getPackageManager().getLaunchIntentForPackage("com.fantasmosoft.free_memory_recover");
+                        if (intent12 != null) {
+                            startActivity(intent12);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装清理哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+
+                    case 13:
+
+                        Intent intent13 = getPackageManager().getLaunchIntentForPackage("com.tencent.weread");
+                        if (intent13 != null) {
+                            startActivity(intent13);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装微信读书哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 14:
+
+                        Intent intent14 = getPackageManager().getLaunchIntentForPackage("com.google.android.voicesearch");
+                        if (intent14 != null) {
+                            startActivity(intent14);
+                        } else {
+                            // 没有安装要跳转的app应用，提醒一下
+                            Toast.makeText(getApplicationContext(), "还没安装语音搜索哟，检查一下吧~", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    case 15:
+
+                        Intent intent15 = new Intent(MoreToolsActivity.this,ApplicationGetActivity.class);
+
+                        startActivity(intent15);
+
+                        break;
+                }
                 Toast.makeText(MoreToolsActivity.this, text, Toast.LENGTH_SHORT).show();
             }
         });
--- app/src/main/java/com/jack/carebaby/ui/OlderLocateActivity.java
+++ app/src/main/java/com/jack/carebaby/ui/OlderLocateActivity.java
@@ -22,28 +22,17 @@ public class OlderLocateActivity extends BasePage {
             @Override
             public void onClick(View v) {
 
-//               Intent intent = new Intent(Maintabs_BActivity.this, Maintabs_AActivity.class);
-//               startActivity(intent);
 
                 Intent intent = getPackageManager().getLaunchIntentForPackage("com.autonavi.minimap");
-// 这里如果intent为空，就说名没有安装要跳转的应用嘛
+
                 if (intent != null) {
-                    // 这里跟Activity传递参数一样的嘛，不要担心怎么传递参数，还有接收参数也是跟Activity和Activity传参数一样
-//                    intent.putExtra("name", "Liu xiang");
-//                    intent.putExtra("birthday", "1983-7-13");
+
                     startActivity(intent);
                 } else {
                     // 没有安装要跳转的app应用，提醒一下
                     Toast.makeText(getApplicationContext(), "哟，赶紧下载安装高德地图吧~", Toast.LENGTH_LONG).show();
                 }
 
-                ////shouhu
-//                aMapLocation.getLocationType();
-//                double a = aMapLocation.getLatitude();//获取纬度
-//                Log.i("纬度", String.valueOf(a));
-//                double b =  aMapLocation.getLongitude();//获取经度
-//                String  friend="2508074836@qq.com";
-//                sendLocation(String.valueOf(a),String.valueOf(b),friend);
 
 
             }
